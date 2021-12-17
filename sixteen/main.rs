a2f689ecb8120c9a0aa8e3e3731306f7b73525a2
timog <timo.glane@gmail.com>
2021-12-17 18:53:42 +0100

    Submitted day sixteen later

--- sixteen/main.rs
+++ sixteen/main.rs
@@ -16,53 +16,110 @@ impl Packet {
         packet
     }
 
-    fn get_version_sum(&self) -> u64 {
-        let mut sum = 0_u64;
+    fn version_sum(&self) -> u64 {
+        let mut sum = self.version as u64;
+        for sub in self.sub_packets.iter() {
+            sum += sub.version_sum();
+        }
         sum
+    }
+
+    fn evaluate(&self) -> u64 {
+        match self.id {
+            4 => {
+                // Literal
+                return self.literal;
+            },
+            0 => {
+                // Sum packet
+                let mut sum = 0;
+                for packet in self.sub_packets.iter() {
+                    sum += packet.evaluate();
+                }
+                return sum;
+            },
+            1 => {
+                // Product packet
+                let mut prod = 1;
+                for packet in self.sub_packets.iter() {
+                    prod *= packet.evaluate();
+                }
+                return prod;
+            },
+            2 => {
+                // Minimum packet
+                let mut min = u64::MAX;
+                for packet in self.sub_packets.iter() {
+                    let val = packet.evaluate();
+                    min = std::cmp::min(val, min);
+                }
+                return min;
+            },
+            3 => {
+                // Maximum packet
+                let mut max = u64::MIN;
+                for packet in self.sub_packets.iter() {
+                    let val = packet.evaluate();
+                    max = std::cmp::max(val, max);
+                }
+                return max;
+            },
+            5 => {
+                // Greater-than packet
+                return (self.sub_packets[0].evaluate() > self.sub_packets[1].evaluate()) as u64;
+            },
+            6 => {
+                // Less-than packet
+                return (self.sub_packets[0].evaluate() < self.sub_packets[1].evaluate()) as u64;
+            },
+            7 => {
+                // Equal-to packet
+                return (self.sub_packets[0].evaluate() == self.sub_packets[1].evaluate()) as u64;
+            }
+            _ => {
+                return 0;
+            }
+        }
     }
 }
 
 fn build_packet(bits: &[u8]) -> Result<(Packet, usize), ()> {
-    println!("Len: {}", bits.len());
     if bits.len() < HEADER_LEN {
-        // Return error?
         return Err(());
     }
 
     let version = (bits[0] << 2) + (bits[1] << 1) + bits[2];
     let id = (bits[3] << 2) + (bits[4] << 1) + bits[5];
 
-    let mut body = &bits[HEADER_LEN..];
+    let body = &bits[HEADER_LEN..];
     match id {
         4 => {
-            // Literal package
-            // Read bundles of 5 bits until the first bit is zero
+            // Literal package - Read bundles of 5 bits until the first bit is zero
             let mut literal = Vec::<u8>::new();
 
             let mut ptr = 0;
-            println!("body len {:?}", body);
-            while ptr + 5 <= body.len() {
-                println!("ptr: {}", ptr);
-                ptr += 5;
+            while ptr <= body.len() - 5 {
+                // Parse a block of 5 bits where the first bit indicats continuation status
                 literal.push(body[ptr + 1]);
                 literal.push(body[ptr + 2]);
                 literal.push(body[ptr + 3]);
                 literal.push(body[ptr + 4]);
+
                 if body[ptr] == 0 {
+                    ptr += 5;
                     break;
                 }
+                ptr += 5;
             }
 
             let parsed_literal = to_num(&literal);
-            println!("Literal: {:?} = {}", literal, parsed_literal);
             return Ok((
                 Packet { version, id, literal: parsed_literal, sub_packets: Vec::<Packet>::new() },
                 ptr + HEADER_LEN
             ));
-
         },
         _ => {
-            // Control package containing sub-packages
+            // Operator package containing sub-packages to be parsed separately
             match body[0] {
                 0 => {
                     if body.len() < 16 {
@@ -73,14 +130,14 @@ fn build_packet(bits: &[u8]) -> Result<(Packet, usize), ()> {
                     let sub_bits = to_num(&body[1..16]) as usize;
                     let mut sub_packets = Vec::<Packet>::new();
 
-                    let mut ptr = 0_usize;
-                    while ptr < sub_bits {
-                        let (packet, end_bit) = build_packet(&body[16+ptr..])?;
+                    let mut ptr = 16_usize;
+                    while ptr < sub_bits + 16 {
+                        let (packet, end_bit) = build_packet(&body[ptr..])?;
                         ptr += end_bit;
                         sub_packets.push(packet);
                     }
 
-                    return Ok((Packet { version, id, literal: 0_u64, sub_packets }, ptr + 16 + HEADER_LEN));
+                    return Ok((Packet { version, id, literal: 0_u64, sub_packets }, ptr + HEADER_LEN));
                 },
                 1 => {
                     if body.len() < 12 {
@@ -91,14 +148,14 @@ fn build_packet(bits: &[u8]) -> Result<(Packet, usize), ()> {
                     let sub_packet_num = to_num(&body[1..12]) as usize;
                     let mut sub_packets = Vec::<Packet>::with_capacity(sub_packet_num);
 
-                    let mut ptr = 0_usize;
+                    let mut ptr = 12_usize;
                     for _ in 0..sub_packet_num {
-                        let (packet, end_bit) = build_packet(&body[12+ptr..])?;
+                        let (packet, end_bit) = build_packet(&body[ptr..])?;
                         ptr += end_bit;
                         sub_packets.push(packet);
                     }
 
-                    return Ok((Packet { version, id, literal: 0_u64, sub_packets }, ptr + 12 + HEADER_LEN));
+                    return Ok((Packet { version, id, literal: 0_u64, sub_packets }, ptr + HEADER_LEN));
                 },
                 _ => {
                     return Err(());
@@ -132,14 +189,13 @@ fn parse_input(filename: &str) -> Result<Vec<u8>, Error> {
 
 fn one(input: &Vec<u8>) {
     let packet = Packet::from(&input);
-    println!("Version: {} - ID: {} - Literal: {}", packet.version, packet.id, packet.literal);
+    println!("ONE: Version sum: {}", packet.version_sum());
+    println!("TWO: Packet evaluates to {}", packet.evaluate());
 }
 
 fn main() {
-    let mut input = parse_input("in.txt")
+    let input = parse_input("in.txt")
         .expect("Failed to parse input");
-
-    println!("Input: {:?}", input);
 
     one(&input);
 }
